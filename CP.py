7a0b8c6fdfbb589a659f395b60971a006935d5b4
Penglin Cai <1402538448@qq.com>
2023-12-27 22:33:35 +0800

    update CP

--- CP.py
+++ CP.py
@@ -3,12 +3,35 @@
 # effect()
 
 
-
 import math
 import queue
 import copy
+import numpy as np
+from TextWorld import TextWorld
 
+NUM_BLOCKS = 8
 
+def ij2k(i, j):
+    idx = -1
+    for m in range(NUM_BLOCKS):
+        for n in range(NUM_BLOCKS):
+            if m == n:
+                continue
+            idx += 1
+            if m == i and n == j:
+                return idx
+    assert False, "should not reach here"
+
+def k2ij(k):
+    idx = -1
+    for i in range(NUM_BLOCKS):
+        for j in range(NUM_BLOCKS):
+            if i == j:
+                continue
+            idx += 1
+            if idx == k:
+                return i, j
+    assert False, "should not reach here"
 
 def distance(sa, sb):
     ret = 0
@@ -17,8 +40,48 @@ def distance(sa, sb):
     
     return math.sqrt(ret)
 
-def effect():
-    return 0
+def effect(action, index, s_cur, sg):
+    obj_A = int(action[4])
+    obj_B = int(action[9])
+    # print(f"[Debug Info] s_cur: {s_cur}, sg: {sg}")
+    if s_cur[0][index] == sg[index]:
+        return 0
+    if index >= 0 and index <= 55:
+        tgt_A, tgt_B = k2ij(index)
+        tgt_A, tgt_B = tgt_A + 1, tgt_B + 1
+        if obj_A != tgt_A:
+            return 0
+        if s_cur[0][index] == 0 and sg[index] == 1:
+            if obj_B == tgt_B:
+                return 1
+            elif obj_B != tgt_B and s_cur[0][ij2k(obj_B-1, tgt_B-1)] == 1:        
+                # obj_B is somehow above tgt_B, indirectly
+                return 1
+            else:
+                return -1
+        elif s_cur[0][index] == 1 and sg[index] == 0:
+            if obj_B == tgt_B:
+                return -1
+            elif obj_B != tgt_B and s_cur[0][ij2k(obj_B-1, tgt_B-1)] == 1:        
+                # obj_B is somehow above tgt_B, indirectly
+                return -1
+            else:
+                return 1
+    elif index >= 56 and index <= 63:
+        tgt_C = index - 55
+        if s_cur[0][index] == 0 and sg[index] == 1:
+            if obj_B != tgt_C and s_cur[0][ij2k(obj_B-1, tgt_C-1)] == 1:
+                # obj_B is somehow above tgt_C, indirectly
+                return -1
+            else:
+                return 1
+        elif s_cur[0][index] == 1 and sg[index] == 0:
+            if obj_B != tgt_C and s_cur[0][ij2k(obj_B-1, tgt_C-1)] == 1:
+                # obj_B is somehow above tgt_C, indirectly
+                return 1
+            else:
+                return 0
+    assert False, "should not reach here"
     
 
 # action checking function, need to import from environment
@@ -29,8 +92,8 @@ def action_is_available(action, state):
 
 
 def precondition(action):
-    g1 = int(action[5])
-    g2 = int(action[10])
+    g1 = int(action[4])
+    g2 = int(action[9])
     return g1, g2
 
 def pi_g(action, s_cur): # get Pi P_{z(s)}(g)
@@ -48,18 +111,18 @@ for i in range(1,9):
     for j in range(1,9):
         if i == j:
             continue
-        string = "pick_" + str(i) + "_on_" + str(j)
+        string = "put_" + str(i) + "_on_" + str(j)
         actions.append(string)
-#['pick_1_on_2', 'pick_1_on_3', ..., 'pick_2_on_1',...,'pick_8_on_7']
+#['put_1_on_2', 'put_1_on_3', ..., 'put_2_on_1',...,'put_8_on_7']
         
 
 # get new state s_new = (state_new, action_history[])
-def state_update(action, s_cur):
+def state_update(action, s_cur, sg):
     new = [0] * 64
     for index in range(0,64):
-        if effect(action, index) == 1:  # positive effect
+        if effect(action, index, s_cur, sg) == 1:  # positive effect
             new[index] = pi_g(action, s_cur[0]) +(1-pi_g(action, s_cur[0])*s_cur[0][index])
-        elif effect(action, index) == -1: # negative effect
+        elif effect(action, index, s_cur, sg) == -1: # negative effect
             new[index] = s_cur[0][index] - pi_g(action, s_cur[0])
         else: # non effect
             new[index] = s_cur[0][index]
@@ -93,7 +156,7 @@ def continous_planner(s0, sg):
 
         # cutting
         states = sorted(states, key=lambda x: distance(x[0],sg))
-        if len(states >= 3):
+        if len(states) >= 3:
             states = copy.deepcopy(states[:3])
         length = len(states)
         
@@ -110,17 +173,41 @@ def continous_planner(s0, sg):
                 if action_is_available(action,s_cur[0]):
                     action_list.append(action)
                     p_a = pi_g(action, s_cur[0])
-                    s_new = state_update(action, s_cur)
+                    s_new = state_update(action, s_cur, sg)
                     states.append(s_new)
 
 
-    states = sorted(states, key=lambda x: distance(x[0], sg))
+    states = sorted(states, key=lambda x: distance(x[0],sg))
     Pi = states[0][1]
 
-
+    print(f"Pi: {Pi}")
     return Pi
 
 
         
-
+if __name__ == "__main__":
+    env = TextWorld()
+    tasks = np.load("./tasks.npy")
+    for episode in range(2000):
+        print(f"task {episode}")
+        env.set_task(tasks[episode])
+        obs, goal, done, reward, info = env.reset(next_task=tasks[episode])
+        step = 0
+        init_description = env.state_to_natual_language()
+        print(f"State: {init_description}")
+        print(f"Goal: {env.vector_to_natural_language(goal)}")
+        while not done and step < 30:
+            step += 1
+            action = continous_planner(obs, goal)
+            print(f"---------- step {step} ----------")
+            print(f"Action: {action}")
+            next_obs, goal, done, reward, info = env.step(action)
+            current_state_description = env.state_to_natual_language()
+            print(f"State: {current_state_description}")
+            print(f"Goal: {env.vector_to_natural_language(goal)}")
+            obs = next_obs
+        if done:
+            print(f">>>>>>>>>>>>>>>>>>>>> done!")
+    env.close()
+    
 
